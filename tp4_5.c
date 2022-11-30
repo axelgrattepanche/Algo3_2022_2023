30c2ccbf60c3772b6fd44765c677b1ed2247130a
axel_grattepanche <113032951+axelgrattepanche@users.noreply.github.com>
2022-11-30 14:24:59 +0100

    Update tp4_5.c

--- tp4_5.c
+++ tp4_5.c
@@ -1,1 +1,19 @@
 
+bool appartientClientFichier(FILE *f, char _nom[15])
+{
+    f=fopen("fich.txt","r+");
+    fseek(f, 0, SEEK_SET);
+    int id =0;
+    char nom[15];
+    while (!feof(f))
+    {
+        fscanf(f,"%d %s", &id, &nom);
+        if (_nom == nom)
+        {
+            printf("le client %s a l'id %d", nom, id);
+            return true;
+        }
+        else
+        return false;
+    }
+}
